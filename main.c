6bdc9bb4d91cc2db10df81e3a72f294a5584d063
Mir <wasim.mir@cerner.com>
2019-09-18 15:44:36 +0530

    adding program 1

---
+++ main.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Program 1 for cerner_2^5_2019
+
+*******************************************************************************/
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int r;
+float pi=3.14,area,ci;
+
+printf("enter radius of circle:");
+scanf("%d",&r);
+area=pi*r*r;
+printf("area  of  circle=%f ",area);
+ci=2*pi*r;
+printf("circumference=%f ",ci);
+getch();
+}
